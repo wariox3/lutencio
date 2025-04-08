b8d70fe750aaff190b3e79578fff9132934a786c
afcanop <afcanop@gmail.com>
2025-04-08 08:38:52 -0500

    fix: ajuste en menuItems

--- constants/menuItems.ts
+++ constants/menuItems.ts
@@ -2,6 +2,6 @@ import { Home, StickyNote } from "@tamagui/lucide-icons";
 import { rutasApp } from "./rutas";
 
 export const menuItems: Record<string, { icon: any; ruta: string }> = {
-    home: { icon: Home, ruta: rutasApp.home },
-    entrega: { icon: StickyNote, ruta: rutasApp.entrega },
+    Home: { icon: Home, ruta: rutasApp.home },
+    Entrega: { icon: StickyNote, ruta: rutasApp.entrega },
   };
