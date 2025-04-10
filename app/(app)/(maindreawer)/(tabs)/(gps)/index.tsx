f22afacc52cb70b2b28744654287efe91c18c657
afcanop <afcanop@gmail.com>
2025-04-10 14:53:22 -0500

    feat: paginación entregas

--- app/(app)/(maindreawer)/(tabs)/(gps)/index.tsx
+++ app/(app)/(maindreawer)/(tabs)/(gps)/index.tsx
@@ -1,22 +1,31 @@
+import Titulo from "@/components/ui/comun/Titulo";
 import Volver from "@/components/ui/navegacion/volver";
-import { Entrega } from "@/interface/entrega/entrega";
-import { obtenerEntregasMapa } from "@/store/selects/entrega";
+import { obtenerEntregasPendientesOrdenadas } from "@/store/selects/entrega";
+import { ArrowLeftCircle, ArrowRightCircle } from "@tamagui/lucide-icons";
 import * as Location from "expo-location";
 import { useNavigation } from "expo-router";
-import React, { useEffect, useState } from "react";
-import { ActivityIndicator, StyleSheet } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
+  StyleSheet,
+} from "react-native";
 import MapView, { Marker, Region } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useSelector } from "react-redux";
-import { H4, View } from "tamagui";
-import MapViewDirections from "react-native-maps-directions";
-import Titulo from "@/components/ui/comun/Titulo";
+import { Button, Card, Text, View, XStack } from "tamagui";
+
+const { width } = Dimensions.get("window");
 
 const Index = () => {
   const navigation = useNavigation();
   const [region, setRegion] = useState<Region | null>(null);
-  const entregasSeleccionadas = useSelector(obtenerEntregasMapa);
+  const entregasSeleccionadas = useSelector(obtenerEntregasPendientesOrdenadas);
   const [coordinates, setCoordinates] = useState<any[]>([]);
+  const flatListRef = useRef<any>(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     navigation.setOptions({
@@ -53,6 +62,19 @@ const Index = () => {
     }
   }, [entregasSeleccionadas]);
 
+  const scrollTo = (direction: "left" | "right") => {
+    let newIndex = currentIndex + (direction === "right" ? 1 : -1);
+
+    // Evitar que se salga del rango
+    newIndex = Math.max(
+      0,
+      Math.min(newIndex, entregasSeleccionadas.length - 1)
+    );
+    setCurrentIndex(newIndex);
+
+    flatListRef.current?.scrollToIndex({ index: newIndex, animated: true });
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
       <Titulo texto="Mapa"></Titulo>
@@ -66,45 +88,78 @@ const Index = () => {
           >
             <Marker coordinate={region} />
 
-            {entregasSeleccionadas.map((entrega: Entrega, key) => (
-              <Marker
-                coordinate={{
-                  latitude: entrega.latitud,
-                  longitude: entrega.longitud,
-                }}
-                key={key.toString()}
-              />
-            ))}
+            <Marker
+              coordinate={{
+                latitude: entregasSeleccionadas[currentIndex].latitud,
+                longitude: entregasSeleccionadas[currentIndex].longitud,
+              }}
+            />
 
-            {entregasSeleccionadas.map((entrega: Entrega, key) => (
-              <MapViewDirections
-                origin={{
-                  latitude: region.latitude,
-                  longitude: region.latitude,
-                }}
-                destination={{
-                  latitude: entrega.latitud,
-                  longitude: entrega.longitud,
-                }}
-                apikey={"AIzaSyDnd8eb9Pq7Dnye_vGeo4MLT389Is_NjzI"}
-                strokeWidth={3}
-                strokeColor="hotpink"
-                optimizeWaypoints={true}
-                key={key.toString()}
-              />
-            ))}
+            <MapViewDirections
+              origin={{
+                latitude: region.latitude,
+                longitude: region.latitude,
+              }}
+              destination={{
+                latitude: entregasSeleccionadas[currentIndex].latitud,
+                longitude: entregasSeleccionadas[currentIndex].longitud,
+              }}
+              apikey={"AIzaSyDnd8eb9Pq7Dnye_vGeo4MLT389Is_NjzI"}
+              strokeWidth={3}
+              strokeColor="hotpink"
+              optimizeWaypoints={true}
+            />
           </MapView>
         ) : (
           <ActivityIndicator size="large" style={styles.loader} />
         )}
       </View>
+      <XStack
+        flex={0.1}
+        my={"$1"}
+        justify={"space-between"}
+        alignItems="center"
+        gap={"$4"}
+      >
+        <Button
+          onPress={() => scrollTo("left")}
+          disabled={currentIndex === 0}
+          variant="outlined"
+          icon={<ArrowLeftCircle size={"$2"}></ArrowLeftCircle>}
+        ></Button>
+        <FlatList
+          ref={flatListRef}
+          data={entregasSeleccionadas}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Card width={width} padding={16}>
+              <Text>ID: {item.id}</Text>
+            </Card>
+          )}
+          horizontal
+          pagingEnabled
+          scrollEnabled={false}
+          showsHorizontalScrollIndicator={false}
+        />
+        <View>
+          <Text>
+            {currentIndex + 1} de {entregasSeleccionadas.length}
+          </Text>
+        </View>
+        <Button
+          onPress={() => scrollTo("right")}
+          disabled={currentIndex === entregasSeleccionadas.length - 1}
+          variant="outlined"
+          icon={<ArrowRightCircle size={"$2"}></ArrowRightCircle>}
+        ></Button>
+      </XStack>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   mapContainer: {
-    flex: 1, // ocupa todo el espacio restante
+    flex: 0.9,
     width: "100%",
   },
   map: {
