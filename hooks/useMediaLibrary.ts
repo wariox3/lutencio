8b716c6a3078bb89111423de729beba1948ed59a
andres <afcanop@gmail.com>
2025-04-28 12:53:28 -0500

    feat: mejora en use media librery

--- hooks/useMediaLibrary.ts
+++ hooks/useMediaLibrary.ts
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import * as MediaLibrary from "expo-media-library";
-import { Alert } from "react-native";
+import * as FileSystem from 'expo-file-system';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const useMediaLibrary = () => {
   const [isDeleting, setIsDeleting] = useState(false);
@@ -49,4 +50,91 @@ export const useMediaLibrary = () => {
   };
 
   return { deleteFileFromGallery, isDeleting, error };
+};
+
+
+export const useEliminarEnGaleria = () => {
+  const [eliminando, setEliminando] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const eliminarArchivo = async (uri: string) => {
+    setEliminando(true);
+    setError(null);
+
+    try {
+      // 1. Comprobar si el archivo existe
+      const archivoInfo = await FileSystem.getInfoAsync(uri);
+      if (!archivoInfo.exists) {
+        throw new Error("El archivo no existe");
+      }
+
+      // 2. Eliminar el archivo
+      await FileSystem.deleteAsync(uri);
+      return true;
+    } catch (err: any) {
+      setError(err.message);
+      return false;
+    } finally {
+      setEliminando(false);
+    }
+  };
+
+  return {
+    eliminarArchivo,
+    eliminando,
+    error,
+  };
+};
+
+
+export const useGuardarEnGaleria = () => {
+  const [guardando, setGuardando] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  // Aquí añadirías tu función para guardar un archivo en la galería
+  const guardarArchivo = async (uri: string) => {
+    setGuardando(true);
+    setError(null);
+    try {
+      // 1. Verificar permisos
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      if (status !== "granted") {
+        throw new Error("Se requieren permisos para acceder a la galería");
+      }
+      // 2. Comprobar si la carpeta existe
+      const subdominio = await AsyncStorage.getItem("subdominio");
+      const rutaCarpeta = FileSystem.documentDirectory + subdominio! + "/";
+
+      const carpetaInfo = await FileSystem.getInfoAsync(rutaCarpeta);
+      if (!carpetaInfo.exists) {
+        await FileSystem.makeDirectoryAsync(rutaCarpeta, { intermediates: true });
+      }
+
+      // 3. Definir el nombre del nuevo archivo
+      const nombreArchivo = uri.split("/").pop();
+      const nuevaRuta = rutaCarpeta + nombreArchivo;
+
+      // 4. Copiar el archivo
+      await FileSystem.copyAsync({
+        from: uri,
+        to: nuevaRuta,
+      });
+
+      console.log();
+      
+      return nuevaRuta; // Devuelve la nueva ruta por si la necesitas
+
+    } catch (error: any) {
+      setError(error.message);
+      return false;
+    } finally {
+      setGuardando(false);
+    }
+  }
+
+  return {
+    guardarArchivo,
+    guardando,
+    error,
+  };
 };
