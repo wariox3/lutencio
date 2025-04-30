958423abdb957a6daa24025246abd237bb1d2d91
andres <afcanop@gmail.com>
2025-04-30 15:21:18 -0500

    refactor ajuste en rutas me navegacion

--- constants/menuItems.ts
+++ constants/menuItems.ts
@@ -3,5 +3,5 @@ import { rutasApp } from "./rutas";
 
 export const menuItems: Record<string, { icon: any; ruta: string }> = {
     Inicio: { icon: Home, ruta: rutasApp.home },
-    Visitas: { icon: StickyNote, ruta: rutasApp.entrega },
+    Visitas: { icon: StickyNote, ruta: rutasApp.visitas },
   };
--- constants/rutas.ts
+++ constants/rutas.ts
@@ -2,7 +2,11 @@ export const rutasApp = {
   login: "/(app)/(login)",
   crearCuenta: "/(app)/(login)/crearCuenta",
   olvidoClave: '/(app)/(login)/olvidoClave',
-  home: "/(app)/(maindreawer)/(tabs)/(inicio)",
+  home: "/(app)/(tabs)/inicio",
+  visitas: "/(app)/(visitas)/lista",
+
+
+  //TODO: ELIMINAR ESTAS RUTAS Y CAMBIAR DONDE SE USEN
   entrega: "/entrega",
   entregaCargar: "/(app)/(maindreawer)/entregaCargar",
   entregaFormulario: "/(app)/(maindreawer)/entregaFormulario",
