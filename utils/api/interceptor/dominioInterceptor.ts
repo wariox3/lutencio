dfeeb2a706bda718f4157adedec68fc3b34f24d1
Sebastian <sebastian.h.piedrahita@gmail.com>
2025-04-30 13:10:30 -0500

    fix: error thunk

--- utils/api/interceptor/dominioInterceptor.ts
+++ utils/api/interceptor/dominioInterceptor.ts
@@ -3,7 +3,8 @@ import { obtenerConfiguracionModoPrueba } from "@/store/selects/configuracion";
 import { InternalAxiosRequestConfig } from "axios";
 
 export const dominioInterceptor = (config: InternalAxiosRequestConfig) => {
-  const modoPrueba = obtenerConfiguracionModoPrueba(store.getState());
+  // const modoPrueba = obtenerConfiguracionModoPrueba(store.getState());
+  const modoPrueba = true 
   if (config.url && modoPrueba) {
     config.url = config.url.replace("reddocapi.online", "reddocapi.online");
   }
