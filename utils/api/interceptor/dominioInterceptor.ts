777823e952a996a403a060b78db7baa9a5a2ea01
sebastian <sebastian.h.piedrahita@gmail.com>
2025-04-30 11:04:29 -0500

    fix: store selector

--- utils/api/interceptor/dominioInterceptor.ts
+++ utils/api/interceptor/dominioInterceptor.ts
@@ -1,10 +1,9 @@
-import { store } from "@/store";
+import { store } from "@/src/application/store";
 import { obtenerConfiguracionModoPrueba } from "@/store/selects/configuracion";
 import { InternalAxiosRequestConfig } from "axios";
 
 export const dominioInterceptor = (config: InternalAxiosRequestConfig) => {
-  // const modoPrueba = obtenerConfiguracionModoPrueba(store.getState());
-  const modoPrueba = true;
+  const modoPrueba = obtenerConfiguracionModoPrueba(store.getState());
   if (config.url && modoPrueba) {
     config.url = config.url.replace("reddocapi.online", "reddocapi.online");
   }
