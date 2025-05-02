1cbeadd08e4b64fa50816cef7c24ed21d2d7ae31
andres <afcanop@gmail.com>
2025-05-02 09:35:15 -0500

    fix: validación de token en use login

--- src/modules/auth/application/view-models/use-login.view-model.ts
+++ src/modules/auth/application/view-models/use-login.view-model.ts
@@ -1,10 +1,12 @@
 import { useAppDispatch, useAppSelector } from "@/src/application/store/hooks";
-import { useRouter } from "expo-router";
+import { useFocusEffect, useRouter } from "expo-router";
 import { useForm } from "react-hook-form";
 import { LoginFormType } from "../../domain/types/login.types";
 import { loginThunk } from "../slices/auth.thunk";
 import { obtenerConfiguracionModoPrueba } from "@/store/selects/configuracion";
 import { useSelector } from "react-redux";
+import { useCallback } from "react";
+import storageService from "@/src/core/services/storage.service";
 
 export const useLoginViewModel = () => {
   const modoPrueba = useSelector(obtenerConfiguracionModoPrueba);
@@ -25,15 +27,37 @@ export const useLoginViewModel = () => {
     },
   });
 
+
+
   const submit = async (data: LoginFormType) => {
     try {
       await dispatch(
         loginThunk({ username: data.username, password: data.password })
       ).unwrap();
-      router.replace("/(app)");
+      handleNagevarApp()
     } catch (error) {
     }
   };
+
+
+  useFocusEffect(
+    useCallback( () => {
+      validarToken()
+    }, [])
+  );
+
+
+  const validarToken = async () => {
+    const token = await storageService.getAuthToken();
+    if (token) {
+      handleNagevarApp()
+    }
+  }
+  
+  const handleNagevarApp = () => {
+    router.replace("/(app)");
+
+  }
 
   const handleNavegarRegistrarse = () => {
     reset();
