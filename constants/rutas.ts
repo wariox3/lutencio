3ff08e547adb99845ee6ae2d7eee7d82cc47815e
andres <afcanop@gmail.com>
2025-05-02 11:18:58 -0500

    fix: se eliminian rutas no necesarias

--- constants/rutas.ts
+++ constants/rutas.ts
@@ -8,15 +8,4 @@ export const rutasApp = {
   vistaPendiente: "/(app)/(visitas)/pendiente",
   visitaEntregar: "/(app)/(visitas)/entregar",
   visitaNovedad: "/(app)/(visitas)/novedad",
-
-
-  //TODO: ELIMINAR ESTAS RUTAS Y CAMBIAR DONDE SE USEN
-  entrega: "/entrega",
-  entregaCargar: "/(app)/(maindreawer)/entregaCargar",
-  entregaFormulario: "/(app)/(maindreawer)/entregaFormulario",
-  entregaNovedad: "/(app)/(maindreawer)/entregaNovedad",
-  entregaPendientes: "/(app)/(maindreawer)/entregaPendientes",
-  entregaMapa: "/(app)/(maindreawer)/entregaMapa",
-  entregaPendientesDetalle:
-    "/(app)/(maindreawer)/entregaPendientesDetalle/[entregaId]",
 } as const;
