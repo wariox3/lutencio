50b0dfeaf9ce1e405fd7a2f41f069251364832c5
Sebastian <sebastian.h.piedrahita@gmail.com>
2025-05-02 13:21:29 -0500

    refactor: visita formulario

---
+++ src/modules/visita/application/view-model/use-visita-formulario.view-model.ts
@@ -0,0 +1,179 @@
+import { rutasApp } from "@/constants/rutas";
+import { useAppSelector } from "@/src/application/store/hooks";
+import * as MediaLibrary from "expo-media-library";
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import {
+    actualizarFirmaEntrega,
+    agregarImagenEntrega,
+    cambiarEstadoEntrega,
+    quitarEntregaSeleccionada,
+} from "../slice/entrega.slice";
+
+type VisitaFormType = {
+  recibe: string;
+  parentesco: string;
+  numeroIdentificacion: string;
+  celular: string;
+};
+
+export default function useVisitaFormularioViewModel() {
+  const dispatch = useDispatch();
+  const entregasSeleccionadas = useAppSelector(
+    ({ entregas }) => entregas.entregasSeleccionadas || []
+  );
+
+  const router = useRouter();
+  const { control, handleSubmit, reset } = useForm<VisitaFormType>({
+    defaultValues: {
+      recibe: "",
+      parentesco: "",
+      numeroIdentificacion: "",
+      celular: "",
+    },
+  });
+
+  const estadoInicial: {
+    arrImagenes: { uri: string }[];
+    mostrarAnimacionCargando: boolean;
+    ubicacionHabilitada: boolean;
+    activarCamara: boolean;
+    abrirGaleria: boolean;
+    exigeImagenEntrega: boolean;
+    exigeFirmaEntrega: boolean;
+    inhabilitarBtnEntrega: boolean;
+    camaraTipo: string;
+    firmarBase64: string | null;
+    fotoSeleccionada: any[];
+  } = {
+    arrImagenes: [],
+    mostrarAnimacionCargando: false,
+    ubicacionHabilitada: false,
+    activarCamara: false,
+    abrirGaleria: false,
+    exigeImagenEntrega: false,
+    exigeFirmaEntrega: false,
+    inhabilitarBtnEntrega: false,
+    camaraTipo: "",
+    firmarBase64: null,
+    fotoSeleccionada: [],
+  };
+
+  const [state, setState] = useState(estadoInicial);
+
+  useEffect(() => {
+    reiniciarEstadoCompleto();
+  }, [entregasSeleccionadas]);
+
+  const reiniciarEstadoCompleto = () => {
+    setState(estadoInicial);
+    reiniciarState();
+  };
+
+  const reiniciarState = () => {
+    reset({
+      recibe: "",
+      parentesco: "",
+      numeroIdentificacion: "",
+      celular: "",
+    });
+  };
+
+  const actualizarState = (newState: Partial<typeof state>) => {
+    setState((prevState) => ({ ...prevState, ...newState }));
+  };
+
+  const handleCapture = (uri: string) => {
+    actualizarState({
+      arrImagenes: [...state.arrImagenes, { uri }],
+    });
+  };
+
+  const handleFirma = (firma: string) => {
+    actualizarState({
+      firmarBase64: firma,
+    });
+  };
+
+  const removerFoto = async (indexArrImagen: number) => {
+    try {
+      const imagen = state.arrImagenes[indexArrImagen];
+      // Aquí deberías también actualizar tu estado para reflejar la eliminación
+      const newArrImagenes = [...state.arrImagenes];
+      newArrImagenes.splice(indexArrImagen, 1);
+      // Suponiendo que tienes una función para actualizar el estado
+      setState((prev) => ({ ...prev, arrImagenes: newArrImagenes }));
+    } catch (error) {
+      console.error("Error al eliminar el archivo:", error);
+    }
+  };
+
+  const removerFirma = async () => {
+    actualizarState({
+      firmarBase64: null,
+    });
+  };
+
+  const guardarEntrega = async (data: VisitaFormType) => {
+    try {
+      actualizarState({ mostrarAnimacionCargando: true });
+
+      // Guardar fotos en el dispositivo
+      const imagenesGuardadas = await Promise.all(
+        state.arrImagenes.map(async (imagen) => {
+          const asset = await MediaLibrary.createAssetAsync(imagen.uri);
+          return asset.uri;
+        })
+      );
+
+      if (state.firmarBase64 !== null) {
+        const firmaGuardada = await MediaLibrary.createAssetAsync(
+          state.firmarBase64
+        );
+        //guardar firma
+        entregasSeleccionadas.map((entregaId) => {
+          dispatch(
+            actualizarFirmaEntrega({
+              entregaId,
+              firmarBase64: firmaGuardada.uri,
+            })
+          );
+        });
+      }
+
+      // Agregar imágenes a entregas seleccionadas
+      entregasSeleccionadas.forEach((entregaId) => {
+        imagenesGuardadas.forEach((uri) => {
+          dispatch(agregarImagenEntrega({ entregaId, imagen: { uri } }));
+        });
+      });
+
+      // Actualizar el estado de las entregas seleccionadas
+      entregasSeleccionadas.forEach((entrega) => {
+        dispatch(cambiarEstadoEntrega(entrega));
+        dispatch(quitarEntregaSeleccionada(entrega));
+      });
+
+      // Navegar a la pantalla de entrega
+      router.navigate(rutasApp.visitas);
+    } catch (error) {
+      console.error("Error en onLoginPressed:", error);
+    } finally {
+      actualizarState({ mostrarAnimacionCargando: false });
+    }
+  };
+
+  return {
+    control,
+    entregasSeleccionadas,
+    state,
+    handleCapture,
+    handleFirma,
+    removerFirma,
+    handleSubmit,
+    guardarEntrega,
+    removerFoto,
+  };
+}
--- src/modules/visita/presentation/screen/visita-formulario.screen.tsx
+++ src/modules/visita/presentation/screen/visita-formulario.screen.tsx
@@ -14,7 +14,7 @@ import {
   actualizarFirmaEntrega,
   agregarImagenEntrega,
   cambiarEstadoEntrega,
-  quitarEntregaSeleccionada
+  quitarEntregaSeleccionada,
 } from "@/store/reducers/entregaReducer";
 import * as MediaLibrary from "expo-media-library";
 import { useNavigation, useRouter } from "expo-router";
@@ -23,158 +23,170 @@ import { FieldValues, useForm } from "react-hook-form";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useDispatch, useSelector } from "react-redux";
 import { Button, H4, ScrollView, Spinner, Text, View, XStack } from "tamagui";
+import useVisitaFormularioViewModel from "../../application/view-model/use-visita-formulario.view-model";
 
 const VisitaFormularioScreen = () => {
-  const dispatch = useDispatch();
-  const entregasSeleccionadas = useSelector(
-    (state: RootState) => state.entregas.entregasSeleccionadas || []
-  );
-  const router = useRouter();
-  const navigation = useNavigation();
-  const { deleteFileFromGallery, isDeleting, error } = useMediaLibrary();
-  const { control, handleSubmit, reset } = useForm<FieldValues>({
-    defaultValues: {
-      recibe: "",
-      parentesco: "",
-      numeroIdentificacion: "",
-      celular: "",
-    },
-  });
+  const {
+    control,
+    entregasSeleccionadas,
+    guardarEntrega,
+    handleCapture,
+    handleFirma,
+    handleSubmit,
+    removerFirma,
+    state,
+    removerFoto,
+  } = useVisitaFormularioViewModel();
+  // const dispatch = useDispatch();
+  // const entregasSeleccionadas = useSelector(
+  //   (state: RootState) => state.entregas.entregasSeleccionadas || []
+  // );
+  // const router = useRouter();
+  // const navigation = useNavigation();
+  // const { deleteFileFromGallery, isDeleting, error } = useMediaLibrary();
+  // const { control, handleSubmit, reset } = useForm<FieldValues>({
+  //   defaultValues: {
+  //     recibe: "",
+  //     parentesco: "",
+  //     numeroIdentificacion: "",
+  //     celular: "",
+  //   },
+  // });
 
-  const estadoInicial: {
-    arrImagenes: { uri: string }[];
-    mostrarAnimacionCargando: boolean;
-    ubicacionHabilitada: boolean;
-    activarCamara: boolean;
-    abrirGaleria: boolean;
-    exigeImagenEntrega: boolean;
-    exigeFirmaEntrega: boolean;
-    inhabilitarBtnEntrega: boolean;
-    camaraTipo: string;
-    firmarBase64: string | null;
-    fotoSeleccionada: any[];
-  } = {
-    arrImagenes: [],
-    mostrarAnimacionCargando: false,
-    ubicacionHabilitada: false,
-    activarCamara: false,
-    abrirGaleria: false,
-    exigeImagenEntrega: false,
-    exigeFirmaEntrega: false,
-    inhabilitarBtnEntrega: false,
-    camaraTipo: "",
-    firmarBase64: null,
-    fotoSeleccionada: [],
-  };
+  // const estadoInicial: {
+  //   arrImagenes: { uri: string }[];
+  //   mostrarAnimacionCargando: boolean;
+  //   ubicacionHabilitada: boolean;
+  //   activarCamara: boolean;
+  //   abrirGaleria: boolean;
+  //   exigeImagenEntrega: boolean;
+  //   exigeFirmaEntrega: boolean;
+  //   inhabilitarBtnEntrega: boolean;
+  //   camaraTipo: string;
+  //   firmarBase64: string | null;
+  //   fotoSeleccionada: any[];
+  // } = {
+  //   arrImagenes: [],
+  //   mostrarAnimacionCargando: false,
+  //   ubicacionHabilitada: false,
+  //   activarCamara: false,
+  //   abrirGaleria: false,
+  //   exigeImagenEntrega: false,
+  //   exigeFirmaEntrega: false,
+  //   inhabilitarBtnEntrega: false,
+  //   camaraTipo: "",
+  //   firmarBase64: null,
+  //   fotoSeleccionada: [],
+  // };
 
-  const [state, setState] = useState(estadoInicial);
+  // const [state, setState] = useState(estadoInicial);
 
-  useEffect(() => {
-    reiniciarEstadoCompleto();
-  }, [entregasSeleccionadas]);
+  // useEffect(() => {
+  //   reiniciarEstadoCompleto();
+  // }, [entregasSeleccionadas]);
 
-  const reiniciarEstadoCompleto = () => {
-    setState(estadoInicial);
-    reiniciarState();
-  };
+  // const reiniciarEstadoCompleto = () => {
+  //   setState(estadoInicial);
+  //   reiniciarState();
+  // };
 
-  const reiniciarState = () => {
-    reset({
-      recibe: "",
-      parentesco: "",
-      numeroIdentificacion: "",
-      celular: "",
-    });
-  };
+  // const reiniciarState = () => {
+  //   reset({
+  //     recibe: "",
+  //     parentesco: "",
+  //     numeroIdentificacion: "",
+  //     celular: "",
+  //   });
+  // };
 
-  const actualizarState = (newState: Partial<typeof state>) => {
-    setState((prevState) => ({ ...prevState, ...newState }));
-  };
+  // const actualizarState = (newState: Partial<typeof state>) => {
+  //   setState((prevState) => ({ ...prevState, ...newState }));
+  // };
 
-  const handleCapture = (uri: string) => {
-    actualizarState({
-      arrImagenes: [...state.arrImagenes, { uri }],
-    });
-  };
+  // const handleCapture = (uri: string) => {
+  //   actualizarState({
+  //     arrImagenes: [...state.arrImagenes, { uri }],
+  //   });
+  // };
 
-  const handleFirma = (firma: string) => {
-    actualizarState({
-      firmarBase64: firma,
-    });
-  };
+  // const handleFirma = (firma: string) => {
+  //   actualizarState({
+  //     firmarBase64: firma,
+  //   });
+  // };
 
-  const removerFoto = async (indexArrImagen: number) => {
-    try {
-      const imagen = state.arrImagenes[indexArrImagen];
-      // Aquí deberías también actualizar tu estado para reflejar la eliminación
-      const newArrImagenes = [...state.arrImagenes];
-      newArrImagenes.splice(indexArrImagen, 1);
-      // Suponiendo que tienes una función para actualizar el estado
-      setState((prev) => ({ ...prev, arrImagenes: newArrImagenes }));
-    } catch (error) {
-      console.error("Error al eliminar el archivo:", error);
-    }
-  };
+  // const removerFoto = async (indexArrImagen: number) => {
+  //   try {
+  //     const imagen = state.arrImagenes[indexArrImagen];
+  //     // Aquí deberías también actualizar tu estado para reflejar la eliminación
+  //     const newArrImagenes = [...state.arrImagenes];
+  //     newArrImagenes.splice(indexArrImagen, 1);
+  //     // Suponiendo que tienes una función para actualizar el estado
+  //     setState((prev) => ({ ...prev, arrImagenes: newArrImagenes }));
+  //   } catch (error) {
+  //     console.error("Error al eliminar el archivo:", error);
+  //   }
+  // };
 
-  const removerFirma = async () => {
-    actualizarState({
-      firmarBase64: null,
-    });
-  };
+  // const removerFirma = async () => {
+  //   actualizarState({
+  //     firmarBase64: null,
+  //   });
+  // };
 
-  const guardarEntrega = async (data: {
-    recibe: string;
-    parentesco: string;
-    numeroIdentificacion: string;
-    celular: string;
-  }) => {
-    try {
-      actualizarState({ mostrarAnimacionCargando: true });
+  // const guardarEntrega = async (data: {
+  //   recibe: string;
+  //   parentesco: string;
+  //   numeroIdentificacion: string;
+  //   celular: string;
+  // }) => {
+  //   try {
+  //     actualizarState({ mostrarAnimacionCargando: true });
 
-      // Guardar fotos en el dispositivo
-      const imagenesGuardadas = await Promise.all(
-        state.arrImagenes.map(async (imagen) => {
-          const asset = await MediaLibrary.createAssetAsync(imagen.uri);
-          return asset.uri;
-        })
-      );
+  //     // Guardar fotos en el dispositivo
+  //     const imagenesGuardadas = await Promise.all(
+  //       state.arrImagenes.map(async (imagen) => {
+  //         const asset = await MediaLibrary.createAssetAsync(imagen.uri);
+  //         return asset.uri;
+  //       })
+  //     );
 
-      if (state.firmarBase64 !== null) {
-        const firmaGuardada = await MediaLibrary.createAssetAsync(
-          state.firmarBase64
-        );
-        //guardar firma
-        entregasSeleccionadas.map((entregaId) => {
-          dispatch(
-            actualizarFirmaEntrega({
-              entregaId,
-              firmarBase64: firmaGuardada.uri,
-            })
-          );
-        });
-      }
+  //     if (state.firmarBase64 !== null) {
+  //       const firmaGuardada = await MediaLibrary.createAssetAsync(
+  //         state.firmarBase64
+  //       );
+  //       //guardar firma
+  //       entregasSeleccionadas.map((entregaId) => {
+  //         dispatch(
+  //           actualizarFirmaEntrega({
+  //             entregaId,
+  //             firmarBase64: firmaGuardada.uri,
+  //           })
+  //         );
+  //       });
+  //     }
 
-      // Agregar imágenes a entregas seleccionadas
-      entregasSeleccionadas.forEach((entregaId) => {
-        imagenesGuardadas.forEach((uri) => {
-          dispatch(agregarImagenEntrega({ entregaId, imagen: { uri } }));
-        });
-      });
+  //     // Agregar imágenes a entregas seleccionadas
+  //     entregasSeleccionadas.forEach((entregaId) => {
+  //       imagenesGuardadas.forEach((uri) => {
+  //         dispatch(agregarImagenEntrega({ entregaId, imagen: { uri } }));
+  //       });
+  //     });
 
-      // Actualizar el estado de las entregas seleccionadas
-      entregasSeleccionadas.forEach((entrega) => {
-        dispatch(cambiarEstadoEntrega(entrega));
-        dispatch(quitarEntregaSeleccionada(entrega));
-      });
+  //     // Actualizar el estado de las entregas seleccionadas
+  //     entregasSeleccionadas.forEach((entrega) => {
+  //       dispatch(cambiarEstadoEntrega(entrega));
+  //       dispatch(quitarEntregaSeleccionada(entrega));
+  //     });
 
-      // Navegar a la pantalla de entrega
-      router.navigate(rutasApp.visitas);
-    } catch (error) {
-      console.error("Error en onLoginPressed:", error);
-    } finally {
-      actualizarState({ mostrarAnimacionCargando: false });
-    }
-  };
+  //     // Navegar a la pantalla de entrega
+  //     router.navigate(rutasApp.visitas);
+  //   } catch (error) {
+  //     console.error("Error en onLoginPressed:", error);
+  //   } finally {
+  //     actualizarState({ mostrarAnimacionCargando: false });
+  //   }
+  // };
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#ffff" }}>
@@ -214,11 +226,11 @@ const VisitaFormularioScreen = () => {
             keyboardType="numeric"
           />
           <SelectInput
-            name='parentesco'
+            name="parentesco"
             control={control}
-            label='Parentesco'
+            label="Parentesco"
             isRequired={false}
-            placeholder='Seleccionar un parentesco'
+            placeholder="Seleccionar un parentesco"
             data={parentescos}
           />
           <BasicInput
