9eca338c34cf6de2272a67d3966a0e5a17221703
Sebastian <sebastian.h.piedrahita@gmail.com>
2025-05-02 15:21:21 -0500

    feat: cambio de subdomain

--- utils/api/interceptor/dominioInterceptor.ts
+++ utils/api/interceptor/dominioInterceptor.ts
@@ -1,8 +1,9 @@
+import { obtenerConfiguracionModoPrueba } from "@/src/application/selectors/configuracion.selector";
+import { store } from "@/src/application/store";
 import { InternalAxiosRequestConfig } from "axios";
 
 export const dominioInterceptor = (config: InternalAxiosRequestConfig) => {
-  // const modoPrueba = obtenerConfiguracionModoPrueba(store.getState());
-  const modoPrueba = true 
+  const modoPrueba = obtenerConfiguracionModoPrueba(store.getState());
   if (config.url && modoPrueba) {
     config.url = config.url.replace("reddocapi.online", "reddocapi.online");
   }
