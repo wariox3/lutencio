343bc2d77a99c4348190266c8c40a4d3dfe3b62e
andres <afcanop@gmail.com>
2025-05-02 14:05:04 -0500

    feat: mejoras en ui

--- components/ui/entrega/entregasSinElementos.tsx
+++ components/ui/entrega/entregasSinElementos.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Text, View } from 'tamagui'
+import { Card, H6, Text, View } from 'tamagui'
 
 const EntregasSinElementos = () => {
   return (
-    <View mx="$3">
-      <Text>Vincular una orden de entrega</Text>
-    </View>
+    <Card flex={0.1} my={"$2"} theme={"blue"} padding={16} mx={'$4'}>
+      <H6 mb="$2">Información</H6>
+      <Text mb="$4">Vincular una orden de entrega</Text>
+    </Card>
   )
 }
 
