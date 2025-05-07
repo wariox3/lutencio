5ec8c8b449f4e56fa3c196098a8eaa86a0bf68ef
afcanop <afcanop@gmail.com>
2025-05-06 19:51:02 -0500

    refacto en formulario entrega online

--- src/modules/visita/application/view-model/use-visita-formulario.view-model.ts
+++ src/modules/visita/application/view-model/use-visita-formulario.view-model.ts
@@ -1,6 +1,5 @@
 import { rutasApp } from "@/constants/rutas";
 import { useAppDispatch, useAppSelector } from "@/src/application/store/hooks";
-import * as MediaLibrary from "expo-media-library";
 import { useRouter } from "expo-router";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
@@ -8,8 +7,16 @@ import {
   actualizarFirmaEntrega,
   agregarImagenEntrega,
   cambiarEstadoEntrega,
+  cambiarEstadoSinconizado,
   quitarEntregaSeleccionada,
 } from "../slice/entrega.slice";
+import * as Network from "expo-network";
+import { Alert } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { consultarApi } from "@/utils/api";
+import APIS from "@/constants/endpoint";
+import { useGuardarEnGaleria } from "@/hooks/useMediaLibrary";
+import * as FileSystem from "expo-file-system";
 
 type VisitaFormType = {
   recibe: string;
@@ -20,6 +27,8 @@ type VisitaFormType = {
 
 export default function useVisitaFormularioViewModel() {
   const dispatch = useAppDispatch();
+  const { guardarArchivo } = useGuardarEnGaleria();
+
   const entregasSeleccionadas = useAppSelector(
     ({ entregas }) => entregas.entregasSeleccionadas || []
   );
@@ -84,15 +93,24 @@ export default function useVisitaFormularioViewModel() {
     setState((prevState) => ({ ...prevState, ...newState }));
   };
 
-  const handleCapture = (uri: string) => {
+  const handleCapture = async (uri: string) => {
+    //1. guardar la foto en el celular
+    const nuevaUri = await guardarArchivo(uri);
+    if (!nuevaUri) {
+      throw new Error("Error al guardar la imagen");
+    }
     actualizarState({
-      arrImagenes: [...state.arrImagenes, { uri }],
+      arrImagenes: [...state.arrImagenes, { uri: nuevaUri }],
     });
   };
 
-  const handleFirma = (firma: string) => {
+  const handleFirma = async (firma: string) => {
+    const nuevaUri = await guardarArchivo(firma);
+    if (!nuevaUri) {
+      throw new Error("Error al guardar la imagen");
+    }
     actualizarState({
-      firmarBase64: firma,
+      firmarBase64: nuevaUri,
     });
   };
 
@@ -117,51 +135,125 @@ export default function useVisitaFormularioViewModel() {
 
   const guardarEntrega = async (data: VisitaFormType) => {
     try {
+      const networkState = await Network.getNetworkStateAsync();
+      const hayConexion =
+        networkState.isConnected && networkState.isInternetReachable;
+
       actualizarState({ mostrarAnimacionCargando: true });
-
-      // Guardar fotos en el dispositivo
-      const imagenesGuardadas = await Promise.all(
-        state.arrImagenes.map(async (imagen) => {
-          const asset = await MediaLibrary.createAssetAsync(imagen.uri);
-          return asset.uri;
-        })
-      );
-
-      if (state.firmarBase64 !== null) {
-        const firmaGuardada = await MediaLibrary.createAssetAsync(
-          state.firmarBase64
-        );
-        //guardar firma
-        entregasSeleccionadas.map((entregaId) => {
-          dispatch(
-            actualizarFirmaEntrega({
-              entregaId,
-              firmarBase64: firmaGuardada.uri,
-            })
-          );
-        });
+      if (!hayConexion) {
+        await entregaVisitaOffline(data, dispatch, router, rutasApp);
+        return;
       }
-
-      // Agregar imágenes a entregas seleccionadas
-      entregasSeleccionadas.forEach((entregaId) => {
-        imagenesGuardadas.forEach((uri) => {
-          dispatch(agregarImagenEntrega({ entregaId, imagen: { uri } }));
-        });
-      });
-
-      // Actualizar el estado de las entregas seleccionadas
-      entregasSeleccionadas.forEach((entrega) => {
-        dispatch(cambiarEstadoEntrega(entrega));
-        dispatch(quitarEntregaSeleccionada(entrega));
-      });
-
-      // Navegar a la pantalla de entrega
-      router.navigate(rutasApp.visitas);
+      await entregaVisitaOnline(data, dispatch, router, rutasApp);
     } catch (error) {
-      console.error("Error en onLoginPressed:", error);
+      actualizarState({ mostrarAnimacionCargando: false });
     } finally {
       actualizarState({ mostrarAnimacionCargando: false });
     }
+    if (state.firmarBase64 !== null) {
+      const firmaGuardada = await MediaLibrary.createAssetAsync(
+        state.firmarBase64
+      );
+      //guardar firma
+      entregasSeleccionadas.map((entregaId) => {
+        dispatch(
+          actualizarFirmaEntrega({
+            entregaId,
+            firmarBase64: firmaGuardada.uri,
+          })
+        );
+      });
+    }
+  };
+
+  const entregaVisitaOffline = async (
+    data: VisitaFormType,
+    dispatch: any,
+    router: any,
+    rutasApp: any
+  ) => {
+    // Guardar fotos en el dispositivo
+    const imagenesGuardadas = await Promise.all(
+      state.arrImagenes.map(async (imagen) => {
+        const asset = await MediaLibrary.createAssetAsync(imagen.uri);
+        return asset.uri;
+      })
+    );
+
+    if (state.firmarBase64 !== null) {
+      const firmaGuardada = await MediaLibrary.createAssetAsync(
+        state.firmarBase64
+      );
+      //guardar firma
+      entregasSeleccionadas.map((entregaId) => {
+        dispatch(
+          actualizarFirmaEntrega({
+            entregaId,
+            firmarBase64: firmaGuardada.uri,
+          })
+        );
+      });
+    }
+
+    // Agregar imágenes a entregas seleccionadas
+    entregasSeleccionadas.forEach((entregaId) => {
+      imagenesGuardadas.forEach((uri) => {
+        dispatch(agregarImagenEntrega({ entregaId, imagen: { uri } }));
+      });
+    });
+
+    // Actualizar el estado de las entregas seleccionadas
+    entregasSeleccionadas.forEach((entrega) => {
+      dispatch(cambiarEstadoEntrega(entrega));
+      dispatch(quitarEntregaSeleccionada(entrega));
+    });
+
+    Alert.alert(`✅ Éxito`, "Guardado localmente por falta de red");
+    router.navigate(rutasApp.visitas);
+  };
+
+  const entregaVisitaOnline = async (
+    data: VisitaFormType,
+    dispatch: any,
+    router: any,
+    rutasApp: any
+  ) => {
+    await Promise.all(
+      entregasSeleccionadas.map(async (visita: number) => {
+        const subdominio = await AsyncStorage.getItem("subdominio");
+        // Usamos Promise.all para esperar a que todas las imágenes se lean
+        const imagenes = await Promise.all(
+          state.arrImagenes.map(async (imagen) => {
+            const base64 = await FileSystem.readAsStringAsync(imagen.uri, {
+              encoding: FileSystem.EncodingType.Base64,
+            });
+            return { base64: `data:image/jpeg;base64,${base64}` };
+          })
+        );
+
+        let firmaBase64 = null;
+        if (state.firmarBase64 !== null) {
+          firmaBase64 = await FileSystem.readAsStringAsync(state.firmarBase64, {
+            encoding: FileSystem.EncodingType.Base64,
+          });
+          firmaBase64 = `data:image/jpeg;base64,${firmaBase64}`;
+        }
+
+        await consultarApi<any>(
+          APIS.ruteo.visitaEntrega,
+          { ...data, id: visita, imagenes, firma: firmaBase64 },
+          {
+            requiereToken: true,
+            subdominio: subdominio!,
+          }
+        );
+      })
+    );
+    entregasSeleccionadas.forEach((entrega) => {
+      dispatch(cambiarEstadoEntrega(entrega));
+      dispatch(quitarEntregaSeleccionada(entrega));
+    });
+    router.navigate(rutasApp.visitas);
   };
 
   return {
