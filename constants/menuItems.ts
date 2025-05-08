722012e8cea04855babaa6dd992334ee513cf402
andres <afcanop@gmail.com>
2025-05-08 09:32:32 -0500

    fix: se retira ruta visitas de menu de items

--- constants/menuItems.ts
+++ constants/menuItems.ts
@@ -3,5 +3,4 @@ import { rutasApp } from "./rutas";
 
 export const menuItems: Record<string, { icon: any; ruta: string }> = {
     Inicio: { icon: Home, ruta: rutasApp.home },
-    Visitas: { icon: StickyNote, ruta: rutasApp.visitas },
-  };
+};
