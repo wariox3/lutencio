1370d697aa185d9b83f7989fcbf2ee76a698077c
sebastian <sebastian.h.piedrahita@gmail.com>
2025-07-23 13:59:30 -0500

    fix: se eliminan funcionalidades no controladas

--- src/modules/visita/infraestructure/services/penditente.service.ts
+++ src/modules/visita/infraestructure/services/penditente.service.ts
@@ -1,8 +1,6 @@
-import { useAppDispatch } from "@/src/application/store/hooks";
 import APIS from "@/src/core/constants/endpoint.constant";
 import { Entrega } from "@/src/modules/visita/domain/interfaces/vista.interface";
 import { consultarApiFormData } from "@/utils/api";
-import * as FileSystem from "expo-file-system";
 
 export class PenditesService {
   static async sincronizarPenditentes(
@@ -10,31 +8,14 @@ export class PenditesService {
     subdominio: string | null
   ) {
     if (!subdominio) return false;
-    const dispatch = useAppDispatch();
 
-    try {
-      //Usamos Promise.all para esperar a que todas las imágenes se lean
-      const imagenes = await Promise.all(
-        entrega.arrImagenes.map(async (imagen) => {
-          const base64 = await FileSystem.readAsStringAsync(imagen.uri, {
-            encoding: FileSystem.EncodingType.Base64,
-          });
-          return { base64: `data:image/jpeg;base64,${base64}` };
-        })
-      );
-
-      let firmaBase64 = null;
-      if (entrega.firmarBase64 !== null) {
-        firmaBase64 = await FileSystem.readAsStringAsync(entrega.firmarBase64, {
-          encoding: FileSystem.EncodingType.Base64,
-        });
-        firmaBase64 = `data:image/jpeg;base64,${firmaBase64}`;
-      }
-
+    try {      
       const formDataToSend = new FormData();
+
       formDataToSend.append("id", `${entrega.id}`);
       formDataToSend.append("fecha_entrega", entrega.fecha_entrega);
-      entrega.arrImagenes.forEach((archivo: any, index: number) => {
+      
+      entrega.arrImagenes?.forEach((archivo: any, index: number) => {
         // Crear un objeto File-like compatible con FormData
         const file = {
           uri: archivo.uri,
@@ -46,7 +27,9 @@ export class PenditesService {
         formDataToSend.append(`imagenes`, file as any, `image-${index}.jpg`); // Usamos 'as any' para evitar el error de tipo
       });
 
+      
       let filefirma: any = "";
+
       if (entrega.firmarBase64) {
         filefirma = {
           uri: entrega.firmarBase64,
@@ -65,6 +48,7 @@ export class PenditesService {
           entrega.datosAdicionales.recibeNumeroIdentificacion,
         recibeCelular: entrega.datosAdicionales.recibeCelular,
       };
+      
       formDataToSend.append(
         "datos_adicionales",
         JSON.stringify(datosAdicionales)
