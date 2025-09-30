4628660b72bd5ed7e8f1638adb994dc5b644996e
afcanop <afcanop@gmail.com>
2025-09-30 14:19:55 -0500

    refactor: Simplifique la lógica de ahorro de soluciones eliminando el manejo en línea/fuera de línea

--- src/modules/visita/application/view-model/use-visita-novedad-solucion.view-model.ts
+++ src/modules/visita/application/view-model/use-visita-novedad-solucion.view-model.ts
@@ -1,15 +1,9 @@
 import { useAppDispatch } from "@/src/application/store/hooks";
-import { STORAGE_KEYS } from "@/src/core/constants";
-import { alertas } from "@/src/core/constants/alertas.const";
-import networkService from "@/src/core/services/network.service";
-import storageService from "@/src/core/services/storage.service";
-import { mostrarAlertHook } from "@/src/shared/hooks/useAlertaGlobal";
+import { useTemaVisual } from "@/src/shared/hooks/useTemaVisual";
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { actualizarNovedadSolucion, cambiarEstadoNovedadSolucion } from "../slice/entrega.slice";
-import { visitaNovedadSolucionThunk } from "../slice/visita.thunk";
-import { useTemaVisual } from "@/src/shared/hooks/useTemaVisual";
+import { cambiarEstadoNovedad } from "../slice/entrega.slice";
 
 const valoresFormulario: NovedadSolucionFormType = {
   solucion: "",
@@ -42,50 +36,15 @@ export default function useVisitaNovedadSolucionViewModel() {
     setState((prevState) => ({ ...prevState, ...newState }));
   };
 
-  const NovedadSolucionOffline = async (data: NovedadSolucionFormType) => {
-    dispatch(
-      actualizarNovedadSolucion(
-        ({
-          novedad_id: parseInt(novedad_id),
-          entrega_id:parseInt(vistaId),
-         solucion_novedad: data.solucion,
-       })
-    ));
-    dispatch(cambiarEstadoNovedadSolucion(parseInt(vistaId)))
-    mostrarAlertHook({
-      titulo: alertas.titulo.exito,
-      mensaje: alertas.mensaje.guardarRegistroLocal,
-    });  };
-
-  const NovedadSolucionOnline = async (data: NovedadSolucionFormType) => {
-    const subdominio = await storageService.getItem(STORAGE_KEYS.subdominio) as string;
-    if (subdominio) {
-      dispatch(
-        visitaNovedadSolucionThunk({
-          id: parseInt(novedad_id),
-          solucion: data.solucion,
-          visita: parseInt(vistaId),
-        })
-      );
-    }
-  };
 
   const guardarSolucion = async (data: NovedadSolucionFormType) => {
-    try {
-      const hayConexion = await networkService.validarEstadoRed();
-      actualizarState({ mostrarAnimacionCargando: true });
-      if (!hayConexion) {
-        await NovedadSolucionOffline(data);
-        return;
-      }
-
-      await NovedadSolucionOnline(data);
-    } catch (error) {
-      actualizarState({ mostrarAnimacionCargando: false });
-    } finally {
-      actualizarState({ mostrarAnimacionCargando: false });
-      router.back();
-    }
+    dispatch(
+      cambiarEstadoNovedad({
+        entregaId: parseInt(vistaId),
+        nuevoEstado: false,
+      })
+    );
+    router.back();
   };
 
   return {
